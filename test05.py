a8256136f067f249ee665399a92aaf2a9cd77c3b
hyojin-park24 <qkrgywls332@gmail.com>
2021-07-12 17:31:07 +0900

    0712커밋완료

---
+++ test05.py
@@ -0,0 +1,12 @@
+import time
+import Person
+
+#class 객체 object
+number = [10,20,30]
+print(dir(number))
+
+p = Person.Person('Edun', 25)
+print(p.age)
+print(p.name)
+print(p.getAge())
+print(p.total)
